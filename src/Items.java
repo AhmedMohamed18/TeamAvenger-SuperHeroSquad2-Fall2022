69c3cfca24945c2f0c10321708533110e1f7936c
Nero <maryjnoob321@gmail.com>
2022-10-22 16:24:54 -0400

    Added Items methods

--- src/Items.java
+++ src/Items.java
@@ -1,3 +1,57 @@
 public class Items
 {
+    private int itemValue;
+    private String name;
+    private String description;
+    private static String location;
+
+    public Items(String gName, String gDescription, String glocation)
+    {
+        this.name = gName;
+        this.description = gDescription;
+        this.location = glocation;
+    }
+
+    public void Item(String name, String descrip, String location)
+    {
+        name = name;
+        description = descrip;
+        location = location;
+    }
+    public void look()
+    {
+        System.out.println(description);
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getLocation()
+    {
+        return location;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public void setLocation(String location)
+    {
+        location = location;
+    }
+    public void setDescription(String description)
+    {
+        description = description;
+    }
+    public void setName(String name)
+    {
+        name = name;
+    }
+    public int getValue()
+    {
+        return itemValue;
+    }
+    public void setValue(int value)
+    {
+        itemValue = value;
+    }
 }
