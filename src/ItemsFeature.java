150f205c1451ae638f846da9027e74357ad2b06c
Nero <maryjnoob321@gmail.com>
2022-10-23 01:20:30 -0400

    Edited some feature names, still working on it

--- src/ItemsFeature.java
+++ src/ItemsFeature.java
@@ -5,16 +5,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ItemsFeature {
-
+public class ItemsFeature
+{
+    String[] add;
     private ArrayList<Items> items;
 
-    public ItemsFeature() throws FileNotFoundException {
+    public ItemsFeature() throws FileNotFoundException
+    {
         items = new ArrayList<Items>();
         readItems("items.txt");
     }
 
-    public void readItems(String itemFile) throws FileNotFoundException {
+    public void readItems(String itemFile) throws FileNotFoundException
+    {
         FileReader fileReader;
         try {
             fileReader = new FileReader(itemFile);
@@ -23,32 +26,24 @@ public class ItemsFeature {
             input.nextLine();
 
             while (input.hasNextLine()) {
-                String[] tokens = input.nextLine().trim().split("~");
-                ArrayList<String> itemsList = new ArrayList<String>(Arrays.asList(tokens[4].trim().split(",")));
+                String[] add = input.nextLine().trim().split("~");
+                ArrayList<String> itemsList = new ArrayList<String>(Arrays.asList(add[4].trim().split(",")));
 
                 if (itemsList.get(0).equals("0")) {
                     itemsList.clear();
                 }
 
-                String[] roomTokens = tokens[6].trim().split(",");
+                String[] roomAdd = add[12].trim().split(",");
 
-
-                items.add(new Room(tokens[0], Boolean.parseBoolean(tokens[1]), tokens[2], tokens[3], itemsList, tokens[5],
-
-                        roomTokens[0], roomTokens[1], roomTokens[2], roomTokens[3]));
-
+                items.add(new Room(add[1], Boolean.parseBoolean(add[1]), add[2], add[3], items, add[5],add[6]));
+                roomAdd[0], roomAdd[1], roomAdd[2], roomAdd[3], roomAdd[4], roomAdd[5], roomAdd[6], roomAdd[7], roomAdd[8], roomAdd[9], roomAdd[10], roomAdd[11], roomAdd[12]));
             }
-
-
             input.close();
-
             fileReader.close();
-
-
-        } catch (IOException e) {
-
-            throw new FileNotFoundException("Map file not found.");
-
+        }
+        catch (IOException e)
+        {
+            throw new FileNotFoundException("file not found.");
         }
     }
 }
